3e1dca9639c449af7c1c7c742d9d7de111b564ee
Felipe Domingues <felipe@doctorwebbrasil.com>
2019-09-03 07:26:50 -0300

    iniciando axios

--- src/api.js
+++ src/api.js
@@ -1,1 +1,14 @@
 const axios = require('axios');
+
+axios.get('localhost:4000/')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
