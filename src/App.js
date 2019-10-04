4e55c6174eb497195a8e7b8e3e4c04282aaae1bd
Felipe Domingues <felipeeu@yahoo.com.br>
2019-10-04 18:47:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,14 @@ import Input from 'react-materialize/lib/Input';
 
 
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
-      number: null , 
+      number: 20 , 
       payload: [],
-      filteredPayload: {}
+      //filteredPayload: {}
     };
   }
 
@@ -26,24 +25,37 @@ class App extends React.Component {
         axios.get('http://www.mocky.io/v2/5d96653a3b00001100c310d3')
       .then(response => {
         this.setState({payload:response.data})
-       this.setState({filteredPayload: response.data.find(item => item.number === number)})
+        this.setState({filteredPayload: response.data.find(item => item.number === this.state.number)})
       });
+      //this.setState({filteredPayload: this.state.payload.find(item => item.number === this.state.number)})
   }
 
-  // componentDidUpdate() {
-  //   document.title = `Você clicou ${this.state.count} vezes`;
-  // }
+  handle () {
+    
+    axios.get('http://www.mocky.io/v2/5d96653a3b00001100c310d3')
+    .then(response => {
+      this.setState({payload:response.data})
+      this.setState({filteredPayload: response.data.find(item => item.number === this.state.number)})
+    });
+  }
+ 
 
   render() {
-    const {filteredPayload} = this.state
+    const {filteredPayload, loading,payload, number} = this.state
+      console.log("payload: ", payload)
+  console.log("number: ",typeof number)
+  console.log("filter: ", filteredPayload)
+
     return (
        <div className="App">
       <Header />
       <input
         type="number"
         onChange={(event) => {
+          this.setState({number:parseInt(event.target.value)})
+          this.setState({filteredPayload: payload.find(item => item.number === parseInt(event.target.value))})
+          event.preventDefault()
           
-         
         }
 
         }
@@ -54,13 +66,10 @@ class App extends React.Component {
           left: 100,
           width: 280
         }} />
-      {/* <button onClick={() => {
-        setFiltered(payload.find(item => item.number === number))
-        console.log("number: ",number)
-         }}>
+      <button  onClick = {() => this.handle() } >
         button
         </button>
-       */}
+      
       
       {loading ?
         <Preloader flashing size="small" /> :
@@ -83,110 +92,5 @@ class App extends React.Component {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function App() {
-
-//   const [loading, setRequest] = useState(false);
-//   const [number, setNumber] = useState()
-//   const [payload, setPayload] = useState([])
-//   const [filteredPayload, setFiltered] = useState({})
-//   // const responseMock = {
-//   //   student: "Felipe Domingues",
-//   //   element: "Tório",
-//   //   symbol: "Th",
-//   //   number: "90",
-//   //   word: "loucão",
-//   //   artdesc:"a arte criada remete ao elemento urânio a arte criada remete ao elemento urânio a arte criada remete ao elemento urânio",
-//   //   elementdesc:"a arte criada remete ao elemento urânio a arte criada remete ao elemento urânio a arte criada remete ao elemento urânio",
-//   //   avatar: "g"
-//   // }
-//   const handleChange = (number) => {
-//     console.log(number)
-//     // setValue(number)
-
-//   }
-
-//   useEffect(() => {
-
-//     setRequest(true);
-//     axios.get('http://www.mocky.io/v2/5d96653a3b00001100c310d3')
-//       .then(response => {
-//         setPayload(response.data)
-//         //setFiltered(response.data.find(item => item.number === number))
-//       });
-//     setRequest(false)
-
-
-//   }, []);
-
-
-
-
-
-
-//   console.log("payload: ", payload)
-//   console.log("number: ", number)
-//   console.log("filter: ", filteredPayload)
-
-//   //setRequest({payload: payload && payload.find(item => item.number === 13) })
-
-
-
-//   return (
-//     <div className="App">
-//       <Header />
-//       <input
-//         type="number"
-//         onChange={(event) => {
-//           setNumber(event.target.value)
-         
-//         }
-
-//         }
-//         //value={value}
-//         placeholder={"digite o número atômico do elemento"}
-//         style={{
-//           position: "absolute",
-//           left: 100,
-//           width: 280
-//         }} />
-//       <button onClick={() => {
-//         setFiltered(payload.find(item => item.number === number))
-//         console.log("number: ",number)
-//          }}>
-//         button
-//         </button>
-      
-      
-//       {loading ?
-//         <Preloader flashing size="small" /> :
-
-//         <Main
-//           student={filteredPayload && filteredPayload.student}
-//           element={filteredPayload && filteredPayload.element}
-//           symbol={filteredPayload && filteredPayload.symbol}
-//           number={filteredPayload && filteredPayload.number}
-//           word={filteredPayload && filteredPayload.word}
-//           artdesc={filteredPayload && filteredPayload.artdesc}
-//           elementdesc={filteredPayload && filteredPayload.elementdesc}
-//           handleChange={handleChange}
-//           payload={payload}
-//         />
-
-//       }
-//     </div>
-//   );
-// }
 
  export default App;
