086809b0d4b1e8ced6b7e95e7305ac8967fe736a
Felipe Domingues <felipeeu@yahoo.com.br>
2019-10-04 20:26:41 -0300

    Updates

---
+++ precache-manifest.ae5d48a38302d3509e51dde959e1a1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af1b596e44a75acf9ff272d28dcf693",
+    "url": "/app_element_client/index.html"
+  },
+  {
+    "revision": "23ebf3d9b06981e60d4b",
+    "url": "/app_element_client/static/css/main.025bc34b.chunk.css"
+  },
+  {
+    "revision": "52fa003f49c006164a12",
+    "url": "/app_element_client/static/js/2.9c7f148e.chunk.js"
+  },
+  {
+    "revision": "23ebf3d9b06981e60d4b",
+    "url": "/app_element_client/static/js/main.869aab48.chunk.js"
+  },
+  {
+    "revision": "ee40b752717445ebe6d2",
+    "url": "/app_element_client/static/js/runtime~main.ad71cb48.js"
+  },
+  {
+    "revision": "cd53b5122f16939910855d9ebf1f07be",
+    "url": "/app_element_client/static/media/logo.cd53b512.svg"
+  }
+]);
