3b1a01ca379421130061d8f52e4aebf8c43fff98
Felipe Domingues <felipeeu@yahoo.com.br>
2020-03-01 08:22:03 -0300

    refactoring and building a periodic table with css grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './css/App.css';
-import { Header } from './components/header.js'
-import { Main } from './components/main.js'
-import { Preloader } from 'react-materialize'
-import axios from 'axios'
-import Input from 'react-materialize/lib/Input';
-import { isAbsolute } from 'path';
-import {data} from "./data"
-
-
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./css/App.css";
+import { Header } from "./components/header.js";
+import { Main } from "./components/main.js";
+import { Preloader } from "react-materialize";
+import axios from "axios";
+import Input from "react-materialize/lib/Input";
+import { isAbsolute } from "path";
+import { PeriodicTable } from "./components/periodic_table";
+// import {data} from "./data"
 
 class App extends React.Component {
   constructor(props) {
@@ -17,52 +16,68 @@ class App extends React.Component {
     this.state = {
       loading: false,
       number: 1,
-      payload: [],
+      payload: []
       //filteredPayload: {}
     };
   }
 
   componentDidMount() {
-    this.setState({ loading: true })
-    // axios.get('  http://www.mocky.io/v2/5d97c6273400006700f4896a')
-    //   .then(response => {
-    //     this.setState({ payload: response.data })
-    //     this.setState({ filteredPayload: response.data.find(item => item.number === this.state.number) })
-    //   });
-    this.setState({ payload: data })
-    this.setState({ loading: false })
-
+    this.setState({ loading: true });
+    axios.get("http://localhost:4000/elements/").then(response => {
+      console.log("RESPONSE: ", response);
+      this.setState({ payload: response.data });
+      this.setState({
+        filteredPayload: response.data.find(
+          item => item.number === this.state.number
+        )
+      });
+    });
+    // this.setState({ payload: data });
+    this.setState({ loading: false });
   }
 
-
   render() {
-    const { filteredPayload, loading, payload, number } = this.state
+    const { filteredPayload, loading, payload, number } = this.state;
 
     return (
       <div className="App">
         <Header />
         <input
           type="number"
-          onChange={(event) => {
-            this.setState({ number: parseInt(event.target.value) })
-            this.setState({ filteredPayload: payload.find(item => item.number === parseInt(event.target.value)) })
-            event.preventDefault()
-          }
-          }
+          onChange={event => {
+            this.setState({ number: parseInt(event.target.value) });
+            this.setState({
+              filteredPayload: payload.find(
+                item => item.number === parseInt(event.target.value)
+              )
+            });
+            event.preventDefault();
+          }}
           placeholder={"digite o número atômico do elemento"}
           style={{
             position: "absolute",
             left: 100,
             width: 280
-          }} />
+          }}
+        />
+        <PeriodicTable />
 
-        {loading || !number || number > 99 ?
-          <div style={{ display:"flex",justifyContent:"center", position: "absolute", top: "40vh", left: "45vw"}}>
-             <span style={{ fontSize:45 , width: 200 }}>Digite um número atômico válido !</span>
+        {/* {loading || !number || number > 99 ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              position: "absolute",
+              top: "40vh",
+              left: "45vw"
+            }}
+          >
+            <span style={{ fontSize: 45, width: 200 }}>
+              Digite um número atômico válido !
+            </span>
             <Preloader flashing size="small" />
-           
           </div>
-          :
+        ) : (
           <Main
             student={filteredPayload && filteredPayload.student}
             element={filteredPayload && filteredPayload.element}
@@ -71,15 +86,11 @@ class App extends React.Component {
             word={filteredPayload && filteredPayload.word}
             artdesc={filteredPayload && filteredPayload.artdesc}
             elementdesc={filteredPayload && filteredPayload.elementdesc}
-
-
           />
-
-        }
+        )} */}
       </div>
     );
   }
 }
-
 
 export default App;
