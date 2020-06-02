ad617872eb8206da92b7bb83adbea4b419c48f6d
Felipe Domingues <>
2020-06-01 22:50:00 -0300

    migrate API call to api.tsx

--- src/api.tsx
+++ src/api.tsx
@@ -1,14 +1,8 @@
 import axios from 'axios'
 
-axios.get('localhost:4000/')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+export const getElements = (url: string, hook:Function ) => {
+  return axios
+    .get(url)
+    .then(response => hook(response.data))
+    .catch(e => alert("Erro ao tentar carregar elementos  --> " + e));
+};
